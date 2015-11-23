3aadba2be413139f4f9852e3fd1cab944bd5c8f2
Bennett Upton <trollgrr@gmail.com>
2015-11-22 19:25:03 -0600

    sql script should be good now.
    I cleaned and added all foreign keys. Eliminated flight table and
    replaced it with flightleg, that I then just named flight.

--- CSC315Final.sql
+++ CSC315Final.sql
@@ -1,22 +1,17 @@
-# Declarations to initialize database in MySQL
-# mysql -u root -p
 DROP DATABASE if exists FlightHandler;
 CREATE DATABASE FlightHandler;
-#Create user is only necessary in new database
-#CREATE USER 'bupton'@'localhost' IDENTIFIED BY 'password';
-#CREATE USER 'johnogel'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON CSC315A3.* TO 'bupton'@'localhost';
-GRANT ALL PRIVILEGES ON CSC315A3.* TO 'johnogel'@'localhost';
 FLUSH PRIVILEGES;
 USE FlightHandler;
 
 DROP TABLE IF EXISTS reservation;
 CREATE TABLE reservation(
 ReservationID INT(12) UNSIGNED NOT NULL AUTO_INCREMENT,
+Customer_ID INT(12) UNSIGNED,
 PRIMARY KEY(ReservationID),
 ReservationHold BOOL NOT NULL,
 INDEX (ReservationID)
 );
+
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer(
 CustomerID INT(12) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -25,7 +20,8 @@ FirstName VARCHAR(45) NOT NULL,
 LastName VARCHAR(45) NOT NULL,
 HomePhone VARCHAR(17),
 PrimaryCardNo VARCHAR(45),
-MailingAddress VARCHAR(45),
+Mailing_Address INT(12) UNSIGNED,
+Physical_Address INT(12) UNSIGNED,
 SeatPref VARCHAR(5), #AISLE CNTR DOOR WSHC
 MealPref VARCHAR(5), #VEGGI HALAL KOSHR PASTA NOPRF
 AcftPosPref VARCHAR(3), #FRT WNG TAL
@@ -35,8 +31,6 @@ FrequentFlierProgC INT(12) UNSIGNED,
 FrequentFlierProgD INT(12) UNSIGNED
 );
 
-# I like this idea, though we might run slowly with lots multiple queries
-# but it might work better with multiple addresses for each customer
 DROP TABLE IF EXISTS address;
 CREATE TABLE address(
 AddressID INT(12) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -51,17 +45,17 @@ Zip INT (5),
 Country VARCHAR(4)
 );
 
-DROP TABLE IF EXISTS flightleg;
-CREATE TABLE flightleg(
+DROP TABLE IF EXISTS flight;
+CREATE TABLE flight(
 LegStatus BOOL,
 Departure_ICAO VARCHAR(4),
 Destination_ICAO VARCHAR(4),
 DepartureTimeLocal TIME,
 ArrivalTimeLocal TIME,
 _Tail VARCHAR(6),
-Carrier_ID VARCHAR(2),
-Flight_No INT(4) UNSIGNED NOT NULL,
-PRIMARY KEY (Carrier_ID, Flight_No, Departure_ICAO)
+Carrier_ID VARCHAR(2) NOT NULL,
+FlightNo INT(4) UNSIGNED NOT NULL,
+PRIMARY KEY (Carrier_ID, FlightNo, Departure_ICAO)
 );
 
 DROP TABLE IF EXISTS airport;
@@ -69,21 +63,15 @@ CREATE TABLE airport(
 ICAO VARCHAR(4),
 PRIMARY KEY(ICAO),
 IATA VARCHAR(3),
+Mailing_Address INT(12) UNSIGNED,
 FullName VARCHAR(45),
 Phone VARCHAR(17)
-);
-
-DROP TABLE IF EXISTS flight;
-CREATE TABLE flight(
-Carrier_ID VARCHAR(2) NOT NULL,
-FlightNo INT(4) UNSIGNED NOT NULL,
-PRIMARY KEY (Carrier_ID, FlightNo)
 );
 
 DROP TABLE IF EXISTS carrier;
 CREATE TABLE carrier(
 CarrierName VARCHAR(45),
-CarrierID VARCHAR(3),
+CarrierID VARCHAR(2),
 Mailing_Address INT(12) UNSIGNED,
 PRIMARY KEY(CarrierID)
 );
@@ -94,13 +82,6 @@ Mailing_Address INT(12) UNSIGNED,
 PRIMARY KEY (Mailing_Address),
 OfficePhone VARCHAR(17)
 );
-
-CREATE TABLE flight(
-Carrier_ID VARCHAR(2) NOT NULL,
-FlightNo INT(4) UNSIGNED NOT NULL,
-PRIMARY KEY (Carrier_ID, FlightNo)
-);
-
 
 CREATE TABLE aircraft(
 Tail VARCHAR(6),
@@ -142,10 +123,9 @@ ALTER TABLE airport ADD FOREIGN KEY (Mailing_Address) REFERENCES address (Addres
 ALTER TABLE carrier ADD FOREIGN KEY (Mailing_Address) REFERENCES address (AddressID);
 ALTER TABLE carrierOffice ADD FOREIGN KEY (Mailing_Address) REFERENCES address (AddressID);
 ALTER TABLE reservation ADD FOREIGN KEY (Customer_ID) REFERENCES customer (CustomerID);
-ALTER TABLE flightleg ADD FOREIGN KEY (Departure_ICAO) REFERENCES airport (ICAO);
-ALTER TABLE flightleg ADD FOREIGN KEY (Destination_ICAO) REFERENCES airport (ICAO);
-ALTER TABLE flightleg ADD FOREIGN KEY (_Tail) REFERENCES aircraft (Tail);
-ALTER TABLE flightleg ADD FOREIGN KEY (Carrier_ID) REFERENCES carrier (CarrierID);
-ALTER TABLE flightleg ADD FOREIGN KEY (Flight_No) REFERENCES flight (FlightNo);
+ALTER TABLE flight ADD FOREIGN KEY (Departure_ICAO) REFERENCES airport (ICAO);
+ALTER TABLE flight ADD FOREIGN KEY (Destination_ICAO) REFERENCES airport (ICAO);
+ALTER TABLE flight ADD FOREIGN KEY (_Tail) REFERENCES aircraft (Tail);
+ALTER TABLE flight ADD FOREIGN KEY (Carrier_ID) REFERENCES carrier (CarrierID);
 ALTER TABLE charge ADD FOREIGN KEY (Payment_ID) REFERENCES paymentType (PaymentID);
 
