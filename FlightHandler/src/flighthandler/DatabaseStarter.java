9aed3ec7af0188afec588067d1820dba3b83e08a
Bennett Upton <trollgrr@gmail.com>
2015-11-29 20:43:16 -0600

    Removed the giant button
    Now it’s just some classes that fill the tables with data. It’s reading
    everything in properly.

--- FlightHandler/src/flighthandler/DatabaseStarter.java
+++ FlightHandler/src/flighthandler/DatabaseStarter.java
@@ -5,10 +5,78 @@
  */
 package flighthandler;
 
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
- *
+ * To modify Initialize or Populate output, find the .sql file in the 
+ * SRC folder and just add stuff directly into it.
  * @author trollgrr
  */
-class DatabaseStarter {
+public class DatabaseStarter {
+
+    private Connection con;
+    private Statement st;
+    String initName = (System.getProperty("user.dir")+"/CSC315Final.sql");
+    String insertName = (System.getProperty("user.dir")+"/CSC315FinalPop.sql");
+    String line;
+    int count;
+
+    public void Initialize() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "AccessMeHere");
+            st = con.createStatement();
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                FileReader fileReader = new FileReader(initName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                while ((line = bufferedReader.readLine()).isEmpty() == false) 
+                {
+                    stmt.executeUpdate(line);
+                }
+
+                bufferedReader.close();
+            } catch (FileNotFoundException ex) {
+                System.out.println("Unable to open file '" + initName + "'");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
     
+    public void Populate()
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "AccessMeHere");
+            st = con.createStatement();
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                FileReader fileReader = new FileReader(insertName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                while ((line = bufferedReader.readLine()).isEmpty() == false) {
+                    stmt.executeUpdate(line);
+                    count++;
+                }
+                System.out.println("Added "+count+" items");
+                bufferedReader.close();
+            } catch (FileNotFoundException ex) {
+                System.out.println("Unable to open file '" + insertName + "'");
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception e) {
+            System.out.println("Added "+count+" items");
+        }
+    }
 }
--- FlightHandler/src/flighthandler/FlightHandler.java
+++ FlightHandler/src/flighthandler/FlightHandler.java
@@ -15,8 +15,14 @@ public class FlightHandler {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Initializing database...");
+        DatabaseStarter dbase = new DatabaseStarter();
+        dbase.Initialize();
+        System.out.println("Database initialized, populating...");
+        dbase.Populate();
         VisualsManager manager = new VisualsManager();
         manager.initialize();
+        
         // TODO code application logic here
     }
     
