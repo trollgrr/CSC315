05dd25b9f1e1b15e3fabe6a0c4f4aaebf529bb98
Bennett Upton <trollgrr@gmail.com>
2015-11-30 21:35:39 -0600

    no changes really

--- FlightHandler/CSC315Query1.sql
+++ FlightHandler/CSC315Query1.sql
@@ -2,3 +2,4 @@ USE FlightHandler;
 SELECT Carrier_ID, FlightNo, Departure_IATA, Destination_IATA FROM flight;
 SELECT Carrier_ID, FlightNo, DepartureTimeLocal, ArrivalTimeLocal FROM flight WHERE Departure_IATA='DFW' AND Destination_IATA='LAX' AND DepartureDate='2006-12-31';
 SELECT CarrierName
+SELECT COUNT 
