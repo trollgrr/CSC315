02e13c4f45c02f7e149fc3d2ef744f6d6aa46572
Johnogel <jonathanbelaire94@gmail.com>
2015-12-01 01:11:28 -0600

    Updated DDL script

--- FlightHandler/CSC315Final.sql
+++ FlightHandler/CSC315Final.sql
@@ -23,10 +23,8 @@ DROP TABLE IF EXISTS paymentType;
 CREATE TABLE paymentType(Method VARCHAR(45), PaymentID INT(12) UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (PaymentID), CardNo INT(16) UNSIGNED, ExpirationDate DATE, SecurityCode INT(4) UNSIGNED, AccountNo INT(17) UNSIGNED, RoutingNo INT(17) UNSIGNED, CardName VARCHAR(45), IBAN VARCHAR (16), Customer_ID INT(12) UNSIGNED);
 DROP TABLE IF EXISTS charge;
 CREATE TABLE charge(Created DATETIME, AmtRcvd NUMERIC(10,2), AmtPaid NUMERIC(10,2), Payment_ID INT(12) UNSIGNED NOT NULL, PRIMARY KEY (Created, Payment_ID));
-DROP TABLE IF EXISTS frequentFlierGroup;
-CREATE TABLE frequentFlierGroup(Customer_ID INT(12) UNSIGNED NOT NULL, FFGroupID INT(12) UNSIGNED NOT NULL, MembershipName VARCHAR(50) NOT NULL, PRIMARY KEY (FFGroupID));
 DROP TABLE IF EXISTS frequentFlierMembership;
-CREATE TABLE frequentFlierMemberShip(FFGroupID INT(12) UNSIGNED NOT NULL, MembershipName VARCHAR(50) NOT NULL, FFPoints INT(12) UNSIGNED, PRIMARY KEY(FFGroupID, MembershipName), FOREIGN KEY (FFGroupID) REFERENCES frequentFlierGroup(FFGroupID));
+CREATE TABLE frequentFlierMemberShip(CustomerID INT(12) UNSIGNED NOT NULL AUTO_INCREMENT, MembershipName VARCHAR(50) NOT NULL, FFPoints INT(12) UNSIGNED, PRIMARY KEY(CustomerID, MembershipName), FOREIGN KEY (CustomerID) REFERENCES customer(CustomerID));
 ALTER TABLE customer ADD FOREIGN KEY (Physical_Address) REFERENCES address (AddressID);
 ALTER TABLE customer ADD FOREIGN KEY (Mailing_Address) REFERENCES address (AddressID);
 ALTER TABLE airport ADD FOREIGN KEY (Mailing_Address) REFERENCES address (AddressID);
@@ -39,5 +37,5 @@ ALTER TABLE flight ADD FOREIGN KEY (_Tail) REFERENCES aircraft (Tail);
 ALTER TABLE flight ADD FOREIGN KEY (Carrier_ID) REFERENCES carrier (CarrierID);
 ALTER TABLE charge ADD FOREIGN KEY (Payment_ID) REFERENCES paymentType (PaymentID);
 ALTER TABLE reservation ADD FOREIGN KEY (Carrier_ID, Flight_No, Departure_Date) REFERENCES flight (Carrier_ID, FlightNo, DepartureDate);
-ALTER TABLE frequentFlierGroup ADD FOREIGN KEY (Customer_ID) REFERENCES customer (CustomerID);
+
 
