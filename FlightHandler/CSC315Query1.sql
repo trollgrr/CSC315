3dffe25dd7c4f266bc8252efa62cc85f3e748f05
Bennett Upton <trollgrr@gmail.com>
2015-11-30 22:44:02 -0600

    half of the query for #3
    It just counts the flights, but doesn’t print for each airline name,
    which will probably require a join, but might be able to get away with
    a nested query.

--- FlightHandler/CSC315Query1.sql
+++ FlightHandler/CSC315Query1.sql
@@ -1,5 +1,5 @@
 USE FlightHandler;
 SELECT Carrier_ID, FlightNo, Departure_IATA, Destination_IATA FROM flight;
 SELECT Carrier_ID, FlightNo, DepartureTimeLocal, ArrivalTimeLocal FROM flight WHERE Departure_IATA='DFW' AND Destination_IATA='LAX' AND DepartureDate='2006-12-31';
-SELECT CarrierName
-SELECT COUNT 
+
+SELECT COUNT(FlightNo) FROM flight WHERE DepartureDate='2006-12-31' AND Departure_IATA='DFW';
